b88dcce6c2a42b2876b2ca0d70dfedd8799e614b
Max_Tiessler <55029168+MaxTiessler@users.noreply.github.com>
2021-05-15 11:27:29 +0200

    hyperlink

--- app/src/main/java/com/example/joiner/LoginPageActivity.java
+++ app/src/main/java/com/example/joiner/LoginPageActivity.java
@@ -1,15 +1,24 @@
 package com.example.joiner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginPageActivity extends AppCompatActivity {
-
+    private TextView tV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        tV = findViewById(R.id.sign_up_txt);
+        tV.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
