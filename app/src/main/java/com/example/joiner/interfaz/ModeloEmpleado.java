e387b021707275cadae93bfee10d5629af611fe7
KuroHaka <ceschen27@gmail.com>
2021-05-15 13:25:57 +0200

    Empleados swap page

---
+++ app/src/main/java/com/example/joiner/interfaz/ModeloEmpleado.java
@@ -0,0 +1,37 @@
+package com.example.joiner.interfaz;
+
+public class ModeloEmpleado {
+    private int image;
+    private String empleadoName;
+    private String desc;
+
+    public ModeloEmpleado(int image, String empleadoName, String desc) {
+        this.image = image;
+        this.empleadoName = empleadoName;
+        this.desc = desc;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getEmpleadoName() {
+        return empleadoName;
+    }
+
+    public void setTitle(String empleadoName) {
+        this.empleadoName = empleadoName;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
