170a8cdd4b2dbe68acd83334d7173d567a5c5f8a
Xoko14 <xoquinperezb@gmail.com>
2021-05-15 17:38:03 +0200

    firebase is no more

--- app/src/main/java/com/example/joiner/MainActivity.java
+++ app/src/main/java/com/example/joiner/MainActivity.java
@@ -12,9 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.joiner.controlador.Candidato;
-import com.google.firebase.firestore.FirebaseFirestore;
-
 public class MainActivity extends AppCompatActivity {
     private Button but;
     @Override
