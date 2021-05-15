4fd13a843e693767f1b5590cafe3bcc3b66105c5
Max_Tiessler <55029168+MaxTiessler@users.noreply.github.com>
2021-05-15 09:55:58 +0200

    mejorar layouts

--- app/src/main/java/com/example/joiner/MainActivity.java
+++ app/src/main/java/com/example/joiner/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.joiner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login_page);
+        TextView tV = findViewById(R.id.sign_up_txt);
+        String s_up_txt = "Sign Up";
+        SpannableString ss = new SpannableString(s_up_txt);
+        ss.setSpan(new ForegroundColorSpan(Color.BLUE),
+                1,
+                7,
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
     }
 }
